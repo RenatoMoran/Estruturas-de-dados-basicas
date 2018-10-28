a10de2ea207fdb6d27aa3ea63c58c418029f2f1f
Renato Moran <rveiga1992@gmail.com>
2018-10-28 00:09:19 -0300

    Add files via upload

---
+++ Bubble sort/Bubble sort Melhorado 1 versão/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int X[5], n, i, aux;
+
+    // Carregando os números no vetor X:
+
+    cout << "Digite 5 numeros" << endl;
+
+    for( i = 0 ; i < 5 ; i++ ){
+        cin >> X[i];
+    }
+
+    cout << "\nVetor antes da ordenacao com o bubble sort!" << endl;
+
+    for( i = 0 ; i < 5 ; i++ ){
+        cout << X[i] << " ";
+    }
+
+    for( n = 1 ; n <= 4 ; n++ ){
+        for( i = 4 ; i >= n ; i-- ){
+            if( X[i] < X[i-1] ){
+                aux = X[i];
+                X[i] = X[i-1];
+                X[i-1] = aux;
+            }
+        }
+    }
+
+    cout << "\nVetor ordenado com o bubble sort!" << endl;
+
+    for( i = 0 ; i < 5 ; i++ ){
+        cout << X[i] << " ";
+    }
+
+    return 0;
+}
